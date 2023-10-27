422fba7a4a5602d403e55d3befb5a23ff7f60c00
Anqi-Li <li.a.titech@gmail.com>
2023-10-27 18:19:04 +0200

    increased redability

--- annotation_converter/utils.py
+++ annotation_converter/utils.py
@@ -14,13 +14,21 @@ def initialize_openlabel_annotation():
     openlabel_annotation['data']['openlabel']['objects'] = {}
     return openlabel_annotation
 
-def convert_extreme_points_to_bbox(maxX, maxY, minX, minY):
+def convert_extreme_points_to_bbox(object_id, list_of_objects):
+
+    # Extreme Point Bounding Boxes
+    for o in list_of_objects:
+        if o['id'] == object_id:
+            maxX = o['geometry']['coordinates']['maxX']['coordinates'][0]
+            maxY = o['geometry']['coordinates']['maxY']['coordinates'][1]
+            minX = o['geometry']['coordinates']['minX']['coordinates'][0]
+            minY = o['geometry']['coordinates']['minY']['coordinates'][1]
+
     # convert to bounding boxes (bbox)
     x = (minX + maxX)/2
     y = (minY + maxY)/2
     w = maxX - minX
     h = maxY - minY
-
     return x,y,w,h
 
 def populate_bbox_values(object_id, frames_objects, bbox_tuple):
@@ -74,16 +82,8 @@ def convert(kognic_annotation: dict) -> dict:
     for i in range(len(list_of_objects)):
         object_id = list_of_objects[i]['id']
         
-        # populate 'frame' - '0' - 'objects' in openlabel format
-        
-        # Extreme Point Bounding Boxes
-        maxX = list_of_objects[i]['geometry']['coordinates']['maxX']['coordinates'][0]
-        maxY = list_of_objects[i]['geometry']['coordinates']['maxY']['coordinates'][1]
-        minX = list_of_objects[i]['geometry']['coordinates']['minX']['coordinates'][0]
-        minY = list_of_objects[i]['geometry']['coordinates']['minY']['coordinates'][1]
-
         # convert to bounding boxes (bbox)
-        x,y,w,h = convert_extreme_points_to_bbox(maxX, maxY, minX, minY)
+        x,y,w,h = convert_extreme_points_to_bbox(object_id, list_of_objects)
 
         # populate 'bbox' values
         populate_bbox_values(object_id, dict_frames_objects, bbox_tuple=(x,y,w,h))
@@ -94,7 +94,7 @@ def convert(kognic_annotation: dict) -> dict:
         # populate 'text' - 'ObjectType' value
         populate_objecttype_value(object_id, kognic_annotation, dict_frames_objects)
 
-        # populate 'objects' in openlabel format
+        # populate 'objects' 
         populate_objects(object_id, kognic_annotation, dict_objects)
 
     return openlabel_annotation
