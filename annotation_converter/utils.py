bb8781f6e772eb2b021d50e7496c508103713528
Anqi-Li <li.a.titech@gmail.com>
2023-10-27 17:59:19 +0200

    blockout smaller functions to improve readability

--- annotation_converter/utils.py
+++ annotation_converter/utils.py
@@ -1,15 +1,6 @@
-#%%
-
-def convert(kognic_annotation: dict) -> dict:
-    '''
-    convert annotation from kognic to openlabel  format
-        parameters: 
-            kognic_annotation (dict): dictionary of annotation in format
-        returns: 
-            openlabel_annotation (dict): dictionary of annotation in openlabel format
-    '''
-    
-    # initiate the openlabel structure
+# %%
+def initialize_openlabel_annotation():
+    # initiate the openlabel structure with empty content
     openlabel_annotation = {}
     openlabel_annotation['data'] = {
         'openlabel':{
@@ -20,20 +11,71 @@ def convert(kognic_annotation: dict) -> dict:
             }
         }
     }
-
     openlabel_annotation['data']['openlabel']['objects'] = {}
+    return openlabel_annotation
 
-    # loop over each 'features' in kognic_annotation
-    openlabel_annotation 
-    frames_objects = openlabel_annotation['data']['openlabel']['frames']['0']['objects']
-    objects = openlabel_annotation['data']['openlabel']['objects']
+def convert_extreme_points_to_bbox(maxX, maxY, minX, minY):
+    # convert to bounding boxes (bbox)
+    x = (minX + maxX)/2
+    y = (minY + maxY)/2
+    w = maxX - minX
+    h = maxY - minY
 
+    return x,y,w,h
+
+def populate_bbox_values(object_id, frames_objects, bbox_tuple):
+    frames_objects[object_id] = {
+        'object_data': {
+            'bbox': [{
+                'name': f'bbox-{object_id[:8]}',
+                'stream': 'CAM',
+                'val': list(bbox_tuple)
+            }]
+        }
+    }
+    
+def populate_unclear_value(object_id, kognic_annotation, dict_frames_objects):
+    if 'Unclear' in kognic_annotation['shapeProperties'][object_id]['@all'].keys():
+        dict_frames_objects[object_id]['object_data']['boolean'] = [{
+            'name': 'Unclear',
+            'val': kognic_annotation['shapeProperties'][object_id]['@all']['Unclear'],
+        }]
+
+def populate_objecttype_value(object_id, kognic_annotation, dict_frames_objects):
+    if 'ObjectType' in kognic_annotation['shapeProperties'][object_id]['@all'].keys():
+        dict_frames_objects[object_id]['object_data']['text'] = [{
+            'name': 'ObjectType',
+            'val': kognic_annotation['shapeProperties'][object_id]['@all']['ObjectType'],
+        }]
+def populate_objects(object_id, kognic_annotation, dict_objects):
+    dict_objects[object_id] = {
+            'name': object_id,
+            'type': kognic_annotation['shapeProperties'][object_id]['@all']['class'],
+        }
+
+def convert(kognic_annotation: dict) -> dict:
+    '''
+    convert annotation from kognic to openlabel  format
+        parameters: 
+            kognic_annotation (dict): dictionary of annotation in format
+        returns: 
+            openlabel_annotation (dict): dictionary of annotation in openlabel format
+    '''
+    
+    #initiate the openlabel structure with empty content
+    openlabel_annotation = initialize_openlabel_annotation()
+
+    # retrieve 'features' in kognic_annotation
     list_of_objects = kognic_annotation['shapes']['CAM']['features']
+
+    dict_frames_objects = openlabel_annotation['data']['openlabel']['frames']['0']['objects']
+    dict_objects = openlabel_annotation['data']['openlabel']['objects']
+    
     for i in range(len(list_of_objects)):
         object_id = list_of_objects[i]['id']
         
         # populate 'frame' - '0' - 'objects' in openlabel format
-        # populate 'bbox' values
+        
         # Extreme Point Bounding Boxes
         maxX = list_of_objects[i]['geometry']['coordinates']['maxX']['coordinates'][0]
         maxY = list_of_objects[i]['geometry']['coordinates']['maxY']['coordinates'][1]
@@ -41,41 +83,18 @@ def convert(kognic_annotation: dict) -> dict:
         minY = list_of_objects[i]['geometry']['coordinates']['minY']['coordinates'][1]
 
         # convert to bounding boxes (bbox)
-        x = (minX + maxX)/2
-        y = (minY + maxY)/2
-        w = maxX - minX
-        h = maxY - minY
+        x,y,w,h = convert_extreme_points_to_bbox(maxX, maxY, minX, minY)
 
-        frames_objects[object_id] = {
-            'object_data': {
-                'bbox': [{
-                    'name': f'bbox-{object_id[:8]}',
-                    'stream': 'CAM',
-                    'val': [x,y,w,h]
-                }]
-            }
-        }
+        # populate 'bbox' values
+        populate_bbox_values(object_id, dict_frames_objects, bbox_tuple=(x,y,w,h))
 
         # populate 'boolean' - 'Unclear' value
-        if 'Unclear' in kognic_annotation['shapeProperties'][object_id]['@all'].keys():
-            frames_objects[object_id]['object_data']['boolean'] = [{
-                'name': 'Unclear',
-                'val': kognic_annotation['shapeProperties'][object_id]['@all']['Unclear'],
-            }]
+        populate_unclear_value(object_id, kognic_annotation, dict_frames_objects)
 
         # populate 'text' - 'ObjectType' value
-        if 'ObjectType' in kognic_annotation['shapeProperties'][object_id]['@all'].keys():
-            frames_objects[object_id]['object_data']['text'] = [{
-                'name': 'ObjectType',
-                'val': kognic_annotation['shapeProperties'][object_id]['@all']['ObjectType'],
-            }]
+        populate_objecttype_value(object_id, kognic_annotation, dict_frames_objects)
 
         # populate 'objects' in openlabel format
-        objects[object_id] = {
-            'name': object_id,
-            'type': kognic_annotation['shapeProperties'][object_id]['@all']['class'],
-        }
+        populate_objects(object_id, kognic_annotation, dict_objects)
 
     return openlabel_annotation
-
-# %%
